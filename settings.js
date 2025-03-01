d64832f7617a3c2399b481ab5f67aa1761a7163e
dev-patrick-alunya <patrickalunya106@gmail.com>
2025-03-01 20:38:51 +0300

    Added creds.json file for 0115348341

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'botAdmin',
   author: 'Patrick Alunya',
-  botName: "My Bot", // Your bot name
+  botName: "Bot Admin", // Your bot name
   botOwner: 'Patrick Alunya', // Your name
-  ownerNumber: '254751991761', //Your number
+  ownerNumber: '254115348341', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
